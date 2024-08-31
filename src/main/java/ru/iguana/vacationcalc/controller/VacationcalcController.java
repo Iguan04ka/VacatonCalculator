9f0982867728733f9f25734d0e91da7581e21281
Iguan04ka <Iguana_2004@mail.ru>
2024-08-31 18:25:43 +0400

    added implementation for controller and service

--- src/main/java/ru/iguana/vacationcalc/controller/VacationcalcController.java
+++ src/main/java/ru/iguana/vacationcalc/controller/VacationcalcController.java
@@ -1,7 +1,21 @@
 package ru.iguana.vacationcalc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.iguana.vacationcalc.service.VacationcalcService;
 
 @RestController
 public class VacationcalcController {
+    @Autowired
+    private VacationcalcService service;
+
+    @GetMapping("/calculate")
+    public double calculate(
+            @RequestParam double avgSalary,
+            @RequestParam int vacationDays,
+            @RequestParam(required = false) String startDate){
+        return this.service.calculateVacationPay(avgSalary, vacationDays);
+    }
 }
--- src/main/java/ru/iguana/vacationcalc/service/VacationcalcService.java
+++ src/main/java/ru/iguana/vacationcalc/service/VacationcalcService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VacationcalcService {
+    public double calculateVacationPay(double salary, int vacationDays){
+        double dailySalary = salary / 30;
+        double payout = dailySalary * vacationDays;
+        return payout;
+    }
 }
